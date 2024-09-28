8b3f9e81c9ca9b5fab7c0cf437ae17f096b67351
hoangvunghi <hoangvunghi2004@gmail.com>
2024-09-28 08:59:19 +0700

    u

--- FE/src/features/home/components/Overview.tsx
+++ FE/src/features/home/components/Overview.tsx
@@ -1,7 +1,7 @@
-export const OverView=()=>{
+export const OverView = () => {
     return (
-        <div>
-           <h1>heheh</h1>
+        <div className="flex justify-center items-center h-screen">
+            <h1 className="text-2xl font-bold text-center">Sẽ bổ sung trang tổng quan trong tương lai</h1>
         </div>
     )
 }
